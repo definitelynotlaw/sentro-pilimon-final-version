696218f06d89d97f87836e2dd5cc69edb5aeadb3
Law <faaarbeyond@gmail.com>
2026-05-02 07:07:21 +0000

    fix: clean up favicon link in layout

--- sentro-pilimon/app/layout.tsx
+++ sentro-pilimon/app/layout.tsx
@@ -42,6 +42,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${playfair.variable} ${jakartaSans.variable} ${jetbrainsMono.variable}`}>
       <head>
+        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <meta name="theme-color" content="#6B0000" />
