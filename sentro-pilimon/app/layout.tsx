148131d813116523621716c75e5d49fb4fdd6fe6
Law <faaarbeyond@gmail.com>
2026-05-02 21:47:07 +0000

    fix: use PLMun seal as favicon

--- sentro-pilimon/app/layout.tsx
+++ sentro-pilimon/app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${playfair.variable} ${jakartaSans.variable} ${jetbrainsMono.variable}`}>
       <head>
-        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+        <link rel="icon" href="/plmun-seal.png.svg" type="image/svg+xml" />
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <meta name="theme-color" content="#6B0000" />
