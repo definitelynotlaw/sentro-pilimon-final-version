94cf592c25a93c493dfd44c7d10a785c56f2ee68
Law <faaarbeyond@gmail.com>
2026-05-02 22:39:10 +0000

    fix: poster_url setValue to empty string instead of undefined

--- sentro-pilimon/components/dashboard/AnnouncementForm.tsx
+++ sentro-pilimon/components/dashboard/AnnouncementForm.tsx
@@ -271,7 +271,7 @@ export function AnnouncementForm({
                 type="button"
                 onClick={() => {
                   setPosterPreview(null)
-                  setValue('poster_url', undefined)
+                  setValue('poster_url', '')
                 }}
                 className="px-4 py-2 rounded-lg flex items-center gap-2 mx-auto"
                 style={{ backgroundColor: '#9B1C1C', color: 'white' }}
