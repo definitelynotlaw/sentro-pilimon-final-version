efa12a209dadb4ce68ccb88a4f12ad3163f4ceb2
Law <faaarbeyond@gmail.com>
2026-05-02 06:45:57 +0000

    feat: add Sentry error monitoring

---
+++ sentro-pilimon/sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://836a5d7cd6de87cc2795f20847993934@o4511318775693312.ingest.us.sentry.io/4511318894641152",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
